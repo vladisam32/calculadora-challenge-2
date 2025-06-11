0f576235f4fd63918568f32150274f088d5a418d
vladi <vladisam32@hotmail.com>
2025-06-10 23:27:10 -0400

    First commit

--- src/main/java/org/litethinking/practica2/calculadora/AplicacionCalculadora.java
+++ src/main/java/org/litethinking/practica2/calculadora/AplicacionCalculadora.java
@@ -38,7 +38,8 @@ public class AplicacionCalculadora {
 
             DataInput input = new DataInput(valor1, simboloOperacion, valor2);
 
-            BigDecimal resultado = app.getCalculoOperacionMatematica(input.valor1(), input.valor2(), input.simboloOperacion());
+            BigDecimal resultado = app.getCalculoOperacionMatematica(input);
+                    //(input.valor1(), input.valor2(), input.simboloOperacion());
 
             System.out.println("Resultado: " + resultado);
         } catch (Exception e) {
@@ -49,14 +50,15 @@ public class AplicacionCalculadora {
     private record DataInput(String valor1, String simboloOperacion, String valor2) {
     }
 
-    private BigDecimal getCalculoOperacionMatematica(final String valor1, final String valor2, final String simboloOperacion) {
-        ValoresOperacion valores = new ValoresOperacion(valor1, valor2);
+    private BigDecimal getCalculoOperacionMatematica( DataInput input ){
+            //final String valor1, final String valor2, final String simboloOperacion) {
+        ValoresOperacion valores = new ValoresOperacion(input.valor1, input.valor2);
 
            /* Operacion operacion = Operacion.obtenerPorSimbolo(simboloOperacion)
                     .orElseThrow(() -> new IllegalArgumentException("Operación desconocida: " + simboloOperacion));
 
             OperacionMatematica instanciaOperacion = operacion.obtenerInstancia();*/
 
-        return calculadoraServicio.calcular(valores, simboloOperacion);
+        return calculadoraServicio.calcular(valores, input.simboloOperacion);
     }
 }
